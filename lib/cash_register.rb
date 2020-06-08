99d6eb5d71ea6fdb895bb4b12b9af38f1daca214
imperative-genre-4923 <dreama.lin@yale.edu>
2020-06-08 15:08:46 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,11 +11,11 @@ class CashRegister
   def add_item (title, price, quantity = 1)
     quantity.times { @items << title }
     @total += price *quantity
-    
+
   end
 
   def apply_discount
-    if @discount > 0 
+    if @discount > 0
       @total = @total * (1-discount)
       return "After the discount, thte toatla comes to @total."
     else
