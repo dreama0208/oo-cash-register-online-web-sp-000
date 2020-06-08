9096abcd799efbcf44f4c58ea06389ef8ed8cefa
imperative-genre-4923 <dreama.lin@yale.edu>
2020-06-08 15:08:43 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,14 +1,26 @@
 class CashRegister
 
-  attr_accessor :total, :discount, :item
+  attr_accessor :total, :discount, :items
 
   def initialize (discount = 0)
     @total = 0
     @discount = discount
-    @item = item
+    @items = []
   end
 
   def add_item (title, price, quantity = 1)
-
+    quantity.times { @items << title }
+    @total += price *quantity
+    
   end
+
+  def apply_discount
+    if @discount > 0 
+      @total = @total * (1-discount)
+      return "After the discount, thte toatla comes to @total."
+    else
+      return "There is no discount to apply."
+  end
+
+
 end
